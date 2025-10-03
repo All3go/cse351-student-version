a0577716c4aa7f300fbe4e702bd3105bbef2e4ad
Austin Linford <alllego321@gmail.com>
2025-10-02 21:12:09 -0600

    team activity completed

--- lesson_03/team/team.py
+++ lesson_03/team/team.py
@@ -4,53 +4,38 @@ Lesson: L03 team activity
 File:   team.py
 Author: <Add name here>
 
-Purpose: Retrieve Star Wars details from a server
-
-Instructions:
-
-- This program requires that the server.py program be started in a terminal window.
-- The program will retrieve the names of:
-    - characters
-    - planets
-    - starships
-    - vehicles
-    - species
-
-- the server will delay the request by 0.5 seconds
-
-TODO
-- Create a threaded function to make a call to the server where
-  it retrieves data based on a URL.  The function should have a method
-  called get_name() that returns the name of the character, planet, etc...
-- The threaded function should only retrieve one URL.
-- Create a queue that will be used between the main thread and the threaded functions
-
-- Speed up this program as fast as you can by:
-    - creating as many as you can
-    - start them all
-    - join them all
-
+Purpose: Retrieve Star Wars details from a server (step 3 - multiple workers for speed)
 """
 
-from datetime import datetime, timedelta
 import threading
+import queue
 from common import *
 
-# Include cse 351 common Python files
 from cse351 import *
 
 # global
 call_count = 0
 
-def get_urls(film6, kind):
+def get_name(url):
     global call_count
+    item = get_data_from_server(url)
+    call_count += 1
+    if item is None:
+        return None
+    return item.get('name') or item.get('title')
 
-    urls = film6[kind]
-    print(kind)
-    for url in urls:
-        call_count += 1
-        item = get_data_from_server(url)
-        print(f'  - {item['name']}')
+def worker(q):
+    """
+    Worker thread: pulls URLs from the queue until empty, retrieves names, prints them.
+    """
+    while True:
+        try:
+            url = q.get_nowait()
+        except queue.Empty:
+            break
+        name = get_name(url)
+        print(f'  - {name}')
+        q.task_done()
 
 def main():
     global call_count
@@ -60,14 +45,36 @@ def main():
 
     film6 = get_data_from_server(f'{TOP_API_URL}/films/6')
     call_count += 1
-    print_dict(film6)
+    print("Film 6 loaded!")
 
-    # Retrieve people
-    get_urls(film6, 'characters')
-    get_urls(film6, 'planets')
-    get_urls(film6, 'starships')
-    get_urls(film6, 'vehicles')
-    get_urls(film6, 'species')
+    # Gather all URLs (characters, planets, species, vehicles, starships)
+    urls = (
+        film6['characters'] +
+        film6['planets'] +
+        film6['species'] +
+        film6['vehicles'] +
+        film6['starships']
+    )
+
+    # Create queue and fill it
+    q = queue.Queue()
+    for url in urls:
+        q.put(url)
+
+    # Start multiple worker threads
+    threads = []
+    num_threads = 10  # you can experiment with this number
+    for _ in range(num_threads):
+        t = threading.Thread(target=worker, args=(q,))
+        t.start()
+        threads.append(t)
+
+    # Wait until all tasks are done
+    q.join()
+
+    # Wait for all worker threads to finish
+    for t in threads:
+        t.join()
 
     log.stop_timer('Total Time To complete')
     log.write(f'There were {call_count} calls to the server')
